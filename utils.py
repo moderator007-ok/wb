4822209a6b1a4eba3fbe193fcedc3bef9a0b2f44
moderator007-ok <meizharryputtar@gmail.com>
2025-03-18 04:26:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,7 @@
 import os
 import logging
-from pyrogram import Client, filters
 from pyrogram.types import Message
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH")
-FFMPEG_PATH = os.environ.get("FFMPEG_PATH", "ffmpeg")  # Defaults to using 'ffmpeg' from the system PATH
-
-if not BOT_TOKEN or API_ID == 0 or not API_HASH:
-    raise ValueError("Missing required bot configuration. Please set BOT_TOKEN, API_ID, and API_HASH as environment variables.")
-
-app = Client("watermark_robot_2", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+from config import app, BOT_TOKEN, API_ID, API_HASH, FFMPEG_PATH
 
 # Allowed admin IDs
 ALLOWED_ADMINS = [640815756, 5317760109]
